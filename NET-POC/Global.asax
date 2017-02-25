00f82cde4f65fca02facf13d141591e9fad5895a
Alexis <Alexis@DESKTOP-H45RGRA>
2017-02-25 17:55:38 -0500

    Add project files.

---
+++ NET-POC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NET_POC.MvcApplication" Language="C#" %>
---
+++ NET-POC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NET_POC.Startup))]
+namespace NET_POC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
