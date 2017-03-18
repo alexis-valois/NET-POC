90e551d19610944a73cf811b2156df1fcaff446a
Alexis Valois-Adamowicz <alexis.valois@hotmail.com>
2017-03-18 10:59:28 -0400

    removed Owin dependecies

--- NET-POC/Controllers/AccountController.cs
+++ NET-POC/Controllers/AccountController.cs
@@ -1,13 +1,6 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.Owin;
-using Microsoft.Owin.Security;
 using NET_POC.Models;
 
 namespace NET_POC.Controllers
